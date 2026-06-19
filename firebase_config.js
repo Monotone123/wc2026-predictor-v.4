197148a30f30c6891d782f80c9802127eb53631d
Monotone123 <99726832+Monotone123@users.noreply.github.com>
2026-06-19 13:32:46 +0700

    Edit ---> firebase_config

--- firebase_config.js
+++ firebase_config.js
@@ -1,12 +1,12 @@
-﻿// Firebase Configuration for WC2026 Predictor
+// Firebase Configuration for WC2026 Predictor
 const firebaseConfig = {
-  apiKey: " AIzaSyB7RIkGcsWhDfXd-MfUMtcP37HrdniNEEI\,
- authDomain: \wc2026-predictor-8dd1d.firebaseapp.com\,
- databaseURL: \https://wc2026-predictor-8dd1d-default-rtdb.asia-southeast1.firebasedatabase.app\,
- projectId: \wc2026-predictor-8dd1d\,
- storageBucket: \wc2026-predictor-8dd1d.firebasestorage.app\,
- messagingSenderId: \69382354330\,
- appId: \1:69382354330:web:c15c006f6ea80d94bbf623\
+  apiKey: "AIzaSyB7RIkGcsWhDfXd-MfUMtcP37HrdniNEEI",
+  authDomain: "wc2026-predictor-8dd1d.firebaseapp.com",
+  databaseURL: "https://wc2026-predictor-8dd1d-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "wc2026-predictor-8dd1d",
+  storageBucket: "wc2026-predictor-8dd1d.firebasestorage.app",
+  messagingSenderId: "69382354330",
+  appId: "1:69382354330:web:c15c006f6ea80d94bbf623"
 };
 
 // Initialize Firebase
